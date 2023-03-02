47332b084462bda39a80990eaf5b656434d8311b
Fabrizia Percan <fabriziapercan@gmail.com>
2023-03-02 21:12:53 +0100

    cleaned and changed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fabrizia Percan</h1>
+    </div>
+  );
+}
+
+export default App;
